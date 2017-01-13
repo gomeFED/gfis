a39ff64640aa51d8b4842d3b45db4ced79bf1c78
Midday <598535774@qq.com>
2017-01-13 15:01:44 +0800

    添加widget命令

---
+++ command/widget.js
@@ -0,0 +1,10 @@
+'use strict';
+
+exports.name = 'widget install';
+exports.desc = 'install widget by widget config of gfe-config.json';
+exports.register = function(commander) {
+    commander
+        .action(function() {
+
+        });
+};
