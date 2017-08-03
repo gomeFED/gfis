3f84904a4abe621fd7d45b826660ce4c06745f9d
lsling <952543325@qq.com>
2017-08-03 15:16:15 +0800

    use-handlebars-3.x

---
+++ test/use-handlebars-3.x/a.js
@@ -0,0 +1,5 @@
+var template = __inline('tpl/some.html');
+var content = template({
+    name: 'fis3'
+})
+document.write(content)
---
+++ test/use-handlebars-3.x/fis-conf.js
@@ -0,0 +1,19 @@
+//npm install -g fis-parser-handlebars-3.x
+fis.match('tpl/*.html', {
+    rExt: '.js', // from .handlebars to .js 虽然源文件不需要编译，但是还是要转换为 .js 后缀
+    parser: fis.plugin('handlebars-3.x', {
+        //fis-parser-handlebars-3.x option
+    }),
+    release: false // handlebars 源文件不需要编译
+});
+
+fis.match('*', {
+    useCache: false,
+    deploy: fis.plugin('local-deliver', {
+        to: './build'
+    })
+});
+
+fis.match('/build/**', {
+    release: false
+});
